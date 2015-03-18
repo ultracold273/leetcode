6f05170f88d8c666708b0fb16b964a9724c5fb9b
Wei Lx <lx@WeideMacBook-Pro.local>
2015-03-18 23:42:04 +0800

    add 15

---
+++ 15-3Sum.cc
@@ -0,0 +1,25 @@
+/**
+ *  Given an array S of n integers, are there elements
+ *  a, b, c in S such that a + b + c = 0?
+ *  Find all unique triplets in the array which gives
+ *  the sum of zero.
+ *  Note:
+ *  Elements in a triplet (a, b, c) must be in
+ *  non-descending order. (ie. a <= b <= b)
+ *  The solution set must not contain duplicate
+ *  triplets.
+ *  For example, given array S = {-1 0 1 2 -1 -4},
+ *  A solution set is:
+ *  (-1, 0, 1), (-1, -1, 2)
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ class Solution
+ {
+ public:
+ 	vector<vector<int>> threeSum(vector<int> &num) {
+ 		
+ 	} 
+ };
