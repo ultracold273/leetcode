afee4bbf57141f6697dd2040b4827602f3df8d09
ultracold <w_weilix@hotmail.com>
2015-03-31 17:23:02 +0800

    add 22

---
+++ 22-GenerateParentheses.cc
@@ -0,0 +1,20 @@
+/**
+ * Given n pairs of parentheses, write a function
+ * to generate all combinations of well-formed 
+ * parentheses.
+ * For example,
+ * given n = 3, a solution set is:
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
+ */
+
+ #include <iostream>
+ #include <vector>
+ #include <string>
+ using namespace std;
+
+ class Solution {
+ public:
+ 	vector<string> generateParenthesis(int n) {
+ 		
+ 	}
+ }
