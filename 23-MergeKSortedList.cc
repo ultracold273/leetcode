21b9667f6a806332cf84cf7329b59d7eed512389
Wei Lx <lx@WeideMacBook-Pro.local>
2015-04-01 00:17:56 +0800

    fin 22; add 23

---
+++ 23-MergeKSortedList.cc
@@ -0,0 +1,26 @@
+/**
+ * Merge k sorted linked lists and return it as one sorted list. 
+ * Analyze and describe its complexity.
+ */
+
+ /**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	Solution * sol = new Solution();
+	return 0;
+}
