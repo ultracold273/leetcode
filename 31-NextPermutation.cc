406be2fb665eb3f33a1ab1e4ecf2dcd9b950cc68
ultracold <w_weilix@hotmail.com>
2015-04-13 13:04:42 +0800

    add 31

---
+++ 31-NextPermutation.cc
@@ -0,0 +1,29 @@
+/**
+ * Leetcode 31 - Next Permutation
+ * Implement next permutation, which rearranges numbers into the
+ * lexicographically next greater permutation of numbers.
+ * If such arrangements is not possible, it must rearrange it as
+ * the lowest possible order (ie, sorted in ascending order).
+ *
+ * The replacement must be in-place, do not allocate extra memory.
+ *
+ * Here are some examples. Inputs are in the left-hand column and
+ * its corresponding ouputs are in the right-hand column.
+ * 1,2,3 -> 1,3,2
+ * 3,2,1 -> 1,2,3
+ * 1,1,5 -> 1,5,1
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+	public:
+		void nextPermutation(vector<int> &num) {
+		}
+};
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
