fd83349384b41a04a609512e66d56069d611625f
ultracold <w_weilix@hotmail.com>
2015-04-29 13:43:37 +0800

    nearly fin 32

--- 32-LongestValidParenthesis.cc
+++ 32-LongestValidParenthesis.cc
@@ -12,12 +12,57 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 	public:
 		int longestValidParentheses(string s) {
-			return 0;
+			int strSize = s.size();
+			int longestValid = 0;
+			vector<vector<int> > vecLength;
+			vector<int> stkPosLeftParenthesis;
+			for(int i = 0;i < strSize;i++) {
+//				cerr << s[i];
+				if (s[i] == '(') {
+					stkPosLeftParenthesis.push_back(i);
+				}else{
+					if (stkPosLeftParenthesis.size() != 0) {
+						// matched parenthesis
+						int lastPos = stkPosLeftParenthesis.back();
+						stkPosLeftParenthesis.pop_back();
+						// Search and merge
+						vector<vector<int> >::iterator it = vecLength.end();
+						it--;
+						while(it != (--vecLength.begin()) && (*it)[0] > lastPos) {
+							vector<vector<int> >::iterator temp = it;
+							it--;
+							vecLength.erase(temp);
+						}
+						if (it == (--vecLength.begin())) {
+							vector<int> aTuple;
+							aTuple.push_back(lastPos);
+							aTuple.push_back(i - lastPos + 1);
+							vecLength.push_back(aTuple);
+						} else {
+							if ((*it)[0] + (*it)[1] == lastPos) {
+								vector<int> aTuple;
+								aTuple.push_back((*it)[0]);
+								aTuple.push_back((*it)[1] + i - lastPos + 1);
+								vecLength.push_back(aTuple);
+							}
+						}
+					}else{
+						// Unmatched parenthesis
+					}
+				}
+			}
+			for (vector<vector<int> >::iterator it = vecLength.begin();it != vecLength.end();it++) {
+				if((*it)[1] >= longestValid) {
+					longestValid = (*it)[1];
+				}
+			}
+			return longestValid;
 		}
 };
 
@@ -28,5 +73,17 @@ int main(int argc, char * argv[]) {
 
 	string s2(")()())");
 	cout << sol->longestValidParentheses(s2) << endl;
+
+	string s3("()(()");
+	cout << sol->longestValidParentheses(s3) << endl;
+
+	string s4("(()(((()");
+	cout << sol->longestValidParentheses(s4) << endl;
+
+	string s5("()(()");
+	cout << sol->longestValidParentheses(s5) << endl;
+
+	string s6("(()()");
+	cout << sol->longestValidParentheses(s6) << endl;
 	return 0;
 }
