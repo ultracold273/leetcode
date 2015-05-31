c2927ba1fafb850ded23337b5e1310c0d6289521
ultracold <w_weilix@hotmail.com>
2015-05-31 17:16:56 +0800

    add 52

---
+++ 52-N-QueensII.cc
@@ -0,0 +1,16 @@
+/**
+ * Follow up for N-Queens problem.
+ *
+ * Now, instead outputting board configurations, 
+ * return the total number of distinct solutions.
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int totalNQueens(int n) {
+	}
+};
+
