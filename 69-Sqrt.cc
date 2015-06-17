1feff30874ea9b8ae0aa122f2587bde01d3171b8
ultracold <w_weilix@hotmail.com>
2015-06-17 23:10:58 +0800

    add 69, fin 70

---
+++ 69-Sqrt.cc
@@ -0,0 +1,14 @@
+/**
+ * Implement int sqrt(int x).
+ *
+ * Compute and return the square root of x.
+ */
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int mySqrt(int x) {
+	}
+};
