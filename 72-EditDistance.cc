564e0299d7ad27deb7db753b36b9b7f38f786daf
ultracold <w_weilix@hotmail.com>
2015-06-19 20:55:14 +0800

    fin 71;fin72

---
+++ 72-EditDistance.cc
@@ -0,0 +1,24 @@
+/**
+ * Given two words word1 and word2, find the minimum number of steps 
+ * required to convert word1 to word2. (each operation is counted as 
+ * 1 step.)
+ *
+ * You have the following 3 operations permitted on a word:
+ *
+ * a) Insert a character
+ * b) Delete a character
+ * c) Replace a character
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		int word1Length = word1.size();
+		int word2Length = word2.size();
+		vector<vector<int> > mat;
+	}
+};
