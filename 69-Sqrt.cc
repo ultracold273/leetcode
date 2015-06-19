a97bc0f4a11b69fa6b5fc8e6204a52dda051229b
ultracold <w_weilix@hotmail.com>
2015-06-19 12:01:11 +0800

    fin 69

--- 69-Sqrt.cc
+++ 69-Sqrt.cc
@@ -10,5 +10,24 @@ using namespace std;
 class Solution {
 public:
 	int mySqrt(int x) {
+		if (x <= 0) return 0;
+
+		// the sqrt is not greater than x/2 + 1
+		int end = x/2 + 1;
+		int start = 0;
+
+		// binary search
+		while(start <= end) {
+			// Equivalent to (start + end) / 2, prevent overflow
+			int mid = start + (end - start) / 2;
+			long long sq = (long long)mid*(long long)mid;
+			if (sq == x) return mid;
+			if (sq < x) {
+				start = mid + 1;
+			}else {
+				end = mid - 1;
+			}
+		}
+		return end;
 	}
 };
