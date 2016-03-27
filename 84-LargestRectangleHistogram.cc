347b0343eddb725283f0e5ad555a6842df92a118
Lix Wei <w_weilix@hotmail.com>
2016-03-27 22:02:49 +0800

    84

--- 84-LargestRectangleHistogram.cc
+++ 84-LargestRectangleHistogram.cc
@@ -21,10 +21,33 @@
 #include <vector>
 using namespace std;
 
+/*
+ * Invariant: Keep the index in stack whose heights is in 
+ * increasing order.
+ */
+
 class Solution {
 public:
-	int largestRectangleArea(vector<int>& height) {
-		return 0;
+	int largestRectangleArea(vector<int>& heights) {
+		stack<int> s;
+		int res = 0;
+		heights.push_back(0);
+
+		for(int i = 0;i < heights.size();i++) {
+			if (s.empty() || heights[i] >= heights[s.top()]) {
+				s.push(i);
+			}else{
+				while(!s.empty() && heights[i] < heights[s.top()]) {
+					int h = heights[s.top()];
+					s.pop();
+
+					int tmp = h * (s.empty()?i:i - s.top() + 1);
+					res = max(res, tmp);
+				}
+				s.push(i);
+			}
+		}
+		return res;
 	}
 };
 
