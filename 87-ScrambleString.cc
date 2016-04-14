2e858b783004f3cac474fd8c96ce45ad9206e8ae
Lix Wei <w_weilix@hotmail.com>
2016-04-14 21:51:16 +0800

    add 87

---
+++ 87-ScrambleString.cc
@@ -0,0 +1,66 @@
+/**
+ * Leetcode 87 - Scramble String
+ * Given a string s1, we may represent it as a binary tree by partitioning it 
+ * to two non-empty substrings recursively.
+ * 
+ * Below is one possible representation of s1 = "great":
+ * 
+ *     great
+ *    /    \
+ *   gr    eat
+ *  / \    /  \
+ * g   r  e   at
+ *            / \
+ *           a   t
+ * To scramble the string, we may choose any non-leaf node and swap 
+ * its two children.
+ * 
+ * For example, if we choose the node "gr" and swap its two children,
+ * it produces a scrambled string "rgeat".
+ *
+ *     rgeat
+ *    /    \
+ *   rg    eat
+ *  / \    /  \
+ * r   g  e   at
+ *            / \
+ *           a   t
+ * We say that "rgeat" is a scrambled string of "great".
+ *
+ * Similarly, if we continue to swap the children of nodes "eat" and "at", 
+ * it produces a scrambled string "rgtae".
+ *
+ *     rgtae
+ *    /    \
+ *   rg    tae
+ *  / \    /  \
+ * r   g  ta  e
+ *        / \
+ *       t   a
+ * We say that "rgtae" is a scrambled string of "great".
+ * 
+ * Given two strings s1 and s2 of the same length, determine if s2 
+ * is a scrambled string of s1.
+ */
+ 
+ #include <iostream>
+ using namespace std;
+ 
+ class Solution {
+ public:
+     bool isScramble(string s1, string s2) {
+         int s1len = s1.size();
+         int s2len = s2.size();
+         assert(s1len == s2len);
+         if (s1len == 1) {
+             return s1 == s2;
+         }else{
+             
+         }
+     }
+ };
+ 
+ int main() {
+     string s1 = "great";
+     string s2 = "rgeat";
+ }
