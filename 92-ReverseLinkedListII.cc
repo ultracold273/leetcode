4bd415715d8ba5cc9c874a180599ff6204516437
ultracold <w_weilix@hotmail.com>
2016-05-12 19:05:12 +0800

    add 92

---
+++ 92-ReverseLinkedListII.cc
@@ -0,0 +1,34 @@
+/**
+ * Leetcode Q92 - Reverse Linked List - II
+ * Reverse a linked list from position m to n. 
+ * Do it in-place and in one-pass.
+ *
+ * For example:
+ * Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+ *
+ * return 1->4->3->2->5->NULL.
+ *
+ * Note:
+ * Given m, n satisfy the following condition:
+ * 1 <= m <= n <= length of list. 
+ */
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode *head, int m, int n) {
+        return head;
+    }
+};
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
