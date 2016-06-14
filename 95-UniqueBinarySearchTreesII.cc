2be0eb18a8c16166c144d72667dd325d3332b53b
Lix Wei <w_weilix@hotmail.com>
2016-06-15 00:37:55 +0800

    mod 95;

---
+++ 95-UniqueBinarySearchTreesII.cc
@@ -0,0 +1,42 @@
+/**
+ * LeetCode Q95 - Unique Binary Search Trees II
+ * Given an integer n, generate all structurally unique BST's 
+ * (binary search trees) that store values 1...n.
+ *
+ * For example,
+ * Given n = 3, your program should return all 5 unique BST's shown below.
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node
+ */
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	vector<TreeNode *> generateTrees(int n) {
+		if (n == 0) return vector<TreeNode *>();
+		vector<TreeNode *> res;
+        res.push_back(TreeNode(1));
+        vector<TreeNode *> temp;
+        for (int i = 2;i <= n;i++) {
+            
+        }
+        return res;
+	}
+};
