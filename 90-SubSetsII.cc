00d6c2cb96483d395e6cfa8d139c8b7a8328ed56
Lix Wei <w_weilix@hotmail.com>
2016-06-15 17:38:52 +0800

    fin 90;96

--- 90-SubSetsII.cc
+++ 90-SubSetsII.cc
@@ -18,53 +18,59 @@
  * ]
  */
  
- #include <iostream>
- #include <vector>
- using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
  
- class Solution {
- public:
-     vector<vector<int> > subsetsWithDup(vector<int>& nums) {
-         vector<vector<int> > res;
-         vector<int> empty;
-         if (nums.size() == 0) {
-             res.push_back(empty);
-             return res;
-         }else if (nums.size() == 1) {
-             res.push_back(empty);
-             res.push_back(nums);
-             return res;
-         }else {
-             vector<int> newNums(nums.begin() + 1, nums.end());
-             vector<vector<int> > subI = subsetsWithDup(newNums);
-             for (int i = 0;i < subI.size();i++) {
-                 res.push_back(subI[i]);
-                 if (nums[0] != nums[1]) {
-                     subI[i].insert(subI[i].begin(), nums[0]);
-                     res.push_back(subI[i]);
-                 }else{
-                     int cntDup = 0;
-                     while(nums[++cntDup] == nums[0]);
-                     if (subI[i][cntDup - 1] == nums[0]) {
-                         subI[i].insert(subI[i].begin(), nums[0]);
-                         res.push_back(subI[i]);
-                     }
-                 }
-             }
-             return res;
-         }
-     }
- };
+class Solution {
+public:
+	vector<vector<int> > subsetsWithDup(vector<int>& nums) {
+		vector<vector<int> > res;
+		vector<int> empty;
+		if (nums.size() == 0) {
+			res.push_back(empty);
+			return res;
+		}else if (nums.size() == 1) {
+			res.push_back(empty);
+			res.push_back(nums);
+			return res;
+		}else {
+			sort(nums.begin(), nums.end());
+			vector<int> newNums(nums.begin() + 1, nums.end());
+			vector<vector<int> > subI = subsetsWithDup(newNums);
+			if (nums[0] != nums[1]) {
+				for (int i = 0;i < (int) subI.size();i++) {
+					res.push_back(subI[i]);
+					subI[i].insert(subI[i].begin(), nums[0]);
+					res.push_back(subI[i]);
+				}
+			}else {
+				int cntDup = 0;
+				while(cntDup < (int)nums.size() && nums[++cntDup] == nums[0]);
+				cout << cntDup << endl;
+				for (int i = 0;i < (int) subI.size();i++) {
+					res.push_back(subI[i]);
+					if (subI[i].size() >= cntDup - 1 && subI[i][cntDup - 2] == nums[0]) {
+						subI[i].insert(subI[i].begin(), nums[0]);
+						res.push_back(subI[i]);
+					}
+				}
+			}
+			return res;
+		}
+	}
+};
 
 int main(int argc, char * argv[]) {
     Solution *sol = new Solution();
-    int numArr[] = {2, 2};
+    int numArr[] = {1, 2, 2};
     vector<int> nums(numArr, numArr + sizeof(numArr) / sizeof(int));
     vector<vector<int> > res = sol->subsetsWithDup(nums);
     cout << '[' << endl;
-    for(int i = 0;i < res.size();i++) {
+    for(int i = 0;i < (int) res.size();i++) {
         cout << '[';
-        for (int j = 0;j < res[i].size();j++) {
+        for (int j = 0;j < (int) res[i].size();j++) {
             cout << res[i][j] << ' ';
         }
         cout << ']' << endl;
