56f9c83f5894185d96721d0597925a05b337c742
Lix Wei <w_weilix@hotmail.com>
2016-06-21 03:50:06 +0800

    fin108

--- 108-ConvertSortedArrayToBST.cc
+++ 108-ConvertSortedArrayToBST.cc
@@ -21,8 +21,9 @@ using namespace std;
  *
  * First find how many elements are there in the left tree and how
  * many are there on the right tree.
+ *
+ * Either the left tree or the right tree contains the size of 2^n - 1
  * 
- * Determine the depth of the total tree
  */
 
 /**
@@ -37,6 +38,38 @@ struct TreeNode {
 
 class Solution {
 public:
+	int ceilinglog2(int x) {
+		int log = 0;
+		while(x) { x >>= 1; log++; }
+		return log;
+	}
+
+	int findRoot(int st, int ed) {
+		int size = ed - st;
+		/*
+		if (((size + 1) & size) == 0) return st + size / 2;
+		else {
+			int log = ceilinglog2(size - 1);
+			int leftSize = size - 1 - ((1 << log) - 1);
+			int rightSize = size - 1 - leftSize;
+			return st + ((leftSize > rightSize)?leftSize:rightSize);
+		}
+		*/
+		return st + size / 2;
+	}
+
+	TreeNode * sortedArrayToBST_rec(vector<int> &nums, int st, int ed) {
+		if (st == ed) return NULL;
+		int rtIndex = findRoot(st, ed);
+		TreeNode * root = new TreeNode(nums[rtIndex]);
+		TreeNode * leftTree = sortedArrayToBST_rec(nums, st, rtIndex);
+		TreeNode * rightTree = sortedArrayToBST_rec(nums, rtIndex + 1, ed);
+		root->left = leftTree;
+		root->right = rightTree;
+		return root;
+	}
+
 	TreeNode * sortedArrayToBST(vector<int> &nums) {
+		return sortedArrayToBST_rec(nums, 0, nums.size());
 	}
 };
