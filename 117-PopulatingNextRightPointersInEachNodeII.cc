779deba86b90aaafc20d0d047c6e4065f4b556e5
Lix Wei <w_weilix@hotmail.com>
2016-07-05 00:42:59 +0800

    fin 116;add 117

--- 117-PopulatingNextRightPointersInEachNodeII.cc
+++ 117-PopulatingNextRightPointersInEachNodeII.cc
@@ -1,23 +1,13 @@
 /**
- * LeetCode 116 - Populating Next Right Pointers in Each Node
+ * LeetCode 117 - Populating Next Right Pointers in Each Node II
  *
- * Given a binary tree
- * struct TreeLinkNode {
- *   TreeLinkNode *left;
- *   TreeLinkNode *right;
- *   TreeLinkNode *next;
- * }
+ * Follow up for problem "Popluating Next Right Pointers in Each Node".
  *
- * Populate each next pointer to point to its next right node. 
- * If there is no next right node, the next pointer should be set to NULL.
- *
- * Initially, all next pointers are set to NULL.
+ * What if the given tree could be any binary tree? Would you previous
+ * solution still work?
  *
  * Note:
- *
  * You may only use constant extra space.
- * You may assume that it is a perfect binary tree (ie, all leaves are 
- * at the same level, and every parent has two children).
  *
  * For example,
  * Given the following perfect binary tree,
@@ -25,16 +15,16 @@
  *     1
  *    /  \
  *   2    3
- *  / \  / \
- * 4  5  6  7
+ *  / \    \
+ * 4   5    7
  *
  * After calling your function, the tree should look like:
  *
  *      1 -> NULL
  *     /  \
  *    2 -> 3 -> NULL
- *   / \  / \
- *  4->5->6->7 -> NULL
+ *   / \    \
+ *  4-> 5 -> 7 -> NULL
  *
  */
 
